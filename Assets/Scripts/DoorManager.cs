3d9def6a1779c5290fec5f333f9cb9871ab08935
camilolc <kmilo3025@hotmail.com>
2023-10-26 09:31:54 -0500

    update props in environment

--- Assets/Scripts/DoorManager.cs
+++ Assets/Scripts/DoorManager.cs
@@ -8,6 +8,9 @@ public class DoorManager : MonoBehaviour
     public GameObject door2;
     public GameObject door3;
     public GameObject door4;
+    public GameObject door5;
+    public GameObject door6;
+    public GameObject door7;
     void Start()
     {
         
@@ -27,6 +30,25 @@ public class DoorManager : MonoBehaviour
             {
                 door1.transform.rotation = Quaternion.Euler(0f, 80f, 0f);
                 door2.transform.position = new Vector3(door2.transform.position.x + .727f, door2.transform.position.y, door2.transform.position.z);
+            }
+
+            if(_item.name == "Green Book")
+            {
+                door3.transform.rotation = Quaternion.Euler(0f, 80f, 0f);
+                door4.transform.rotation = Quaternion.Euler(0f, 190f, 0f);
+                Debug.Log("Green done");
+            }
+
+            if (_item.name == "Red Book")
+            {
+                door5.transform.rotation = Quaternion.Euler(0f, 80f, 0f);
+                door6.transform.position = new Vector3(door6.transform.position.x + .727f, door6.transform.position.y, door6.transform.position.z);
+            }
+
+            if (_item.name == "Yellow Book")
+            {
+                door7.transform.rotation = Quaternion.Euler(0f, -280f, 0f);
+               
             }
         }
     }
