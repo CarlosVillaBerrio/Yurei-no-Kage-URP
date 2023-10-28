131bbfa03e94e952f864a9b65f5c7570fa53bbec
camilolc <kmilo3025@hotmail.com>
2023-10-27 21:02:27 -0500

    update dialogs

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class ItemObject : MonoBehaviour
 {
     [SerializeField] private ItemData itemData;
+    public TextMeshProUGUI text;
 
     private void OnValidate()
     {
@@ -16,9 +19,33 @@ public class ItemObject : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
+            int random = Random.Range(1, 3);
+
+            switch (random)
+            {
+                case 1:
+                    text.text = "An evidence, yay!";
+                    break;
+                case 2:
+                    text.text = "I found it";
+                    break;
+                case 3:
+                    text.text = "Gotcha";
+                    break;
+            }
+
+            StartCoroutine(TextTrigger());
             Inventory.instance.AddItem(itemData);
 
             Destroy(gameObject,1f);
         }
+    }
+
+    IEnumerator TextTrigger()
+    {
+        text.gameObject.SetActive(true);
+        yield return new WaitForSeconds(0.85f);
+        text.gameObject.SetActive(false);
+
     }
 }
