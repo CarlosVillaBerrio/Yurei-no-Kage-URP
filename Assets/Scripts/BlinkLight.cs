7c438c9b17758a8bbbb48f4fb9499525a594c415
camilolc <kmilo3025@hotmail.com>
2023-11-06 15:34:58 -0500

    update inside rooms lights

--- Assets/Scripts/BlinkLight.cs
+++ Assets/Scripts/BlinkLight.cs
@@ -6,6 +6,9 @@ public class BlinkLight : MonoBehaviour
 {
     public bool blink = false;
     public float timeDelay;
+    public float minRange = 0.02f;
+    public float maxRange = 0.6f;
+
 
 
     void Update()
@@ -19,10 +22,10 @@ public class BlinkLight : MonoBehaviour
     {
         blink = true;
         this.gameObject.GetComponent<Light>().enabled = false;
-        timeDelay = Random.Range(0.02f, 0.6f);
+        timeDelay = Random.Range(minRange, maxRange);
         yield return new WaitForSeconds(timeDelay);
         this.gameObject.GetComponent<Light>().enabled = true;
-        timeDelay = Random.Range(0.02f, 0.6f);
+        timeDelay = Random.Range(minRange, maxRange);
         yield return new WaitForSeconds(timeDelay);
         blink = false;
 
